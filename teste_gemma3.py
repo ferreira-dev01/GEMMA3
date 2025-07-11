8e6a8671cf6e1b2bcb5a7bb86b2a7800bb476110
FerreiraDev <nicoguitarreiro@gmail.com>
2025-07-11 11:27:13 -0300

    atualizacao consulta por url

--- teste_gemma3.py
+++ teste_gemma3.py
@@ -1,20 +1,91 @@
-import ollama
-#  127.0.0.1:11434: bind:
-# inicia o cliente
-client = ollama.Client()
+# import ollama
+import sys
+sys.stdout.reconfigure(encoding="utf-8")
+# # 127.0.0.1:11434: bind:
+# #http://localhost:3000/
+# # inicia o cliente
+# #client = ollama.Client()
+# client = ollama.Client(host='127.0.0.1:11434')
 
-# prompt com contexto
-contexto = """
-    aja como professor.
-"""
+# # prompt com contexto
+# contexto = """
 
-instrucao = """
-        responda a pergunta. quais foram as capitais do brasil?
-"""
+# """
 
-prompt = f"Contexto: {contexto}\n\nInstrução: {instrucao}" # define prompt
+# instrucao = """
+# qual a capital do  equador?
+# """
 
-response = client.generate(model="gemma3:12b", prompt=prompt) # alimento modelo com prompt
+# prompt = f"Contexto: {contexto}\n\nInstrução: {instrucao}" # define prompt
 
-# imprime a resposta
-print("Resposta do Gema 3:", response)
+# response = client.generate(model="gemma3:12b", prompt=prompt) # alimento modelo com prompt
+
+# # imprime a resposta
+# print("Resposta do Gema 3:", response.response)
+
+
+# import pycurl
+
+# def process_header(header_line):
+#     print(header_line.decode('utf-8').strip())
+
+# c = pycurl.Curl()
+# c.setopt(c.URL, 'http://127.0.0.1:11434')
+# c.setopt(c.HEADERFUNCTION, process_header)
+# c.setopt(c.NOBODY, 1)
+# c.perform()
+# c.close()
+
+
+
+# import pycurl
+# from io import BytesIO
+
+# buffer = BytesIO()
+# c = pycurl.Curl()
+# c.setopt(c.URL,  'http://127.0.0.1:11434')
+# c.setopt(c.FOLLOWLOCATION, 1)
+# c.setopt(c.WRITEDATA, buffer)
+# c.perform()
+# c.close()
+# response = buffer.getvalue()
+# print(response.decode("utf-8"))
+
+
+# import ollama
+# response = ollama.chat(model='gemma3:12b', messages=[
+#   {
+#     'role': 'user',
+#     'content': ' quanto é 5 mais 3?',
+#   },
+# ])
+# print(response['message']['content'])
+
+# import ollama
+
+# # Use the generate function for a one-off prompt
+# result = ollama.generate(model='gemma3:12b', prompt='QUAL é sua versão?')
+# print(result['response'])
+
+# from ollama import chat
+
+# conversation = [
+#     {"role": "user", "content": "Hello, how are you?"}
+# ]
+# reply = chat(model='gemma3:12b', messages=conversation)
+# print(reply.message.content)
+
+import requests
+
+SERVER_URL = "http://127.0.0.1:11434/api/generate" # add your server ip
+model = "gemma3:12b"  # or "deepseek"
+prompt="qual a capital do estado de minas gerais que fica no pais brasil? "
+payload = {
+    "model": model,
+    "prompt": prompt,
+    "stream": False
+}
+
+response = requests.post(SERVER_URL, json=payload)
+response_json = response.json()  # Convert response to dictionary
+print(response_json.get("response", "Key 'response' not found"))
